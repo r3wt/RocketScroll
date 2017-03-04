96587a5d2aaae01127916ce526ee7dd0abd8bd8a
r3wt <gmorris_89@outlook.com>
2017-03-04 16:13:03 -0600

    wraps angular module in iife

--- src/ng-rocketscroll.js
+++ src/ng-rocketscroll.js
@@ -1,27 +1,30 @@
-angular
-.module('ng-rocketscroll',[])
-.directive('ngRocketScroll',function($timeout){
-	return {
-		restrict: 'A',
-		link: function(scope,element,attrs){
-			var instance = new RocketScroll(angular.element(element)[0],{ wrapContents: false });// dont wrap element in template. well do it manually
-			var timeout = null;
-			scope.$watch(function(){
-				
-				return element.find('.rs-content')[0].offsetHeight;
-				
+!function(angular){
+	angular
+	.module('ng-rocketscroll',[])
+	.directive('ngRocketScroll',function($timeout){
+		return {
+			restrict: 'A',
+			link: function(scope,element,attrs){
+				//todo parse options for-realzies
+				var instance = new RocketScroll(angular.element(element)[0],{ wrapContents: false });// dont wrap element in template. well do it manually
+				var timeout = null;
+				scope.$watch(function(){
+					
+					return element.find('.rs-content')[0].offsetHeight;
+					
+				},
+				function(){
+					if(timeout != null){
+						$timeout.cancel(timeout);
+						timeout = null;
+					}
+					$timeout(function(){
+						instance.refresh();
+					},100);
+				});
 			},
-			function(){
-				if(timeout != null){
-					$timeout.cancel(timeout);
-					timeout = null;
-				}
-				$timeout(function(){
-					instance.refresh();
-				},100);
-			});
-		},
-		transclude: true,
-		template: '<div class="rs-container"><div class="rs-content" ng-transclude></div></div><div class="rs-scrollbar"><div class="rs-handle"></div>'
-	}
-});
+			transclude: true,
+			template: '<div class="rs-container"><div class="rs-content" ng-transclude></div></div><div class="rs-scrollbar"><div class="rs-handle"></div>'
+		}
+	});	
+}(angular);
